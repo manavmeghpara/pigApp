bf66e3a1b9aa3de59775116120a6b677840f9729
manavmeghpara <manav09meghpara@gmail.com>
2022-11-18 18:18:55 -0800

    report

---
+++ src/app/pig-report/pig-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pig-report',
+  templateUrl: './pig-report.component.html',
+  styleUrls: ['./pig-report.component.css']
+})
+export class PigReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
